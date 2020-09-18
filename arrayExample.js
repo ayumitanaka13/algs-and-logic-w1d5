cfd93645c81301fd3f7b065a98c953fa38bd2126
ayumitanaka <tanakayumi13@gmail.com>
2020-09-18 10:55:46 -0700

    first commit

---
+++ arrayExample.js
@@ -0,0 +1,7 @@
+let cars = ['BMW', 'Audi', 'Ford'];
+
+console.log(cars[0]); //BMW
+
+cars.push('Dodge');
+
+console.log(cars[3]); //Dodge
---
+++ loopingThroughArrays.js
@@ -0,0 +1,12 @@
+let foods = ['apple', 'bread', 'cheese', 'wine'];
+
+console.log(foods[2]); //cheese
+
+for (let i = 0; i < foods.length; i++) {
+    console.log(`${foods[i]} is tasty!`);    
+}
+
+// apple
+// bread
+// cheese
+// wine
